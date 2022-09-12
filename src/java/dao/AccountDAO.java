e22fed8d2bb687afd8ec5bce35486b5c9f745eba
DoopTu <92850092+DoopTu@users.noreply.github.com>
2022-09-12 21:29:58 +0700

    Update AccountDAO.java

--- src/java/dao/AccountDAO.java
+++ src/java/dao/AccountDAO.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dao;
 
 import DBUtils.MyLib;
@@ -12,10 +8,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-/**
- *
- * @author LENOVO
- */
+
 //lop nay de chua cac function thuc hien cac cau query lay data trong DB
 public class AccountDAO {
 
