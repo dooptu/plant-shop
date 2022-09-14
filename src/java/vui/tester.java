c78ef90cc774ee1c6f37f5cfed7b3e38ec902a52
DoopTu <92850092+DoopTu@users.noreply.github.com>
2022-09-14 09:33:28 +0700

    Update tester.java

--- src/java/vui/tester.java
+++ src/java/vui/tester.java
@@ -8,6 +8,7 @@ package vui;
 import basicclass.Account;
 import dao.AccountDAO;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -15,6 +16,7 @@ import java.util.ArrayList;
  */
 public class tester {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
         ArrayList<Account> list = AccountDAO.getAccounts();
         try {
             for (Account account : list) {
@@ -44,15 +46,54 @@ public class tester {
             e.printStackTrace();
         }
         
-        System.out.println("-------------------");
-        list = AccountDAO.insertAccounts( "ha@gmail.com", "123", "hanguyen", "456", 0, 1);
-        try {
-            for (Account account : list) {
-            System.out.println(account.toString());
-        }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        
+//        System.out.println("-------------------");
+//        System.out.println("Insert");
+//        String gmail;
+//        String psw;
+//        String name;
+//        String phone;
+//        int status;
+//        int role;
+//        
+//        System.out.print("Email: ");
+//        gmail = sc.nextLine();
+//        System.out.print("Password: ");
+//        psw = sc.nextLine();
+//        System.out.print("Full name: ");
+//        name = sc.nextLine();
+//        System.out.print("Phone: ");
+//        phone = sc.nextLine();
+//        System.out.print("Status: ");
+//        status = Integer.parseInt(sc.nextLine());
+//        System.out.print("Role: ");
+//        role = Integer.parseInt(sc.nextLine());
+//        
+//        list = AccountDAO.insertAccounts(gmail, psw, name, phone, status, role);
+//        try {
+//            for (Account account : list) {
+//            System.out.println(account.toString());
+//        }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        
+//        System.out.println("-------------------");
+//        System.out.println("Update status");
+//        int idUp;
+//        int statusUp;
+//        System.out.print("Account ID: ");
+//        idUp = Integer.parseInt(sc.nextLine());
+//        System.out.print("Status: ");
+//        statusUp = Integer.parseInt(sc.nextLine());
+//        list = AccountDAO.updateStatus(idUp, statusUp);
+//        try {
+//            for (Account account : list) {
+//            System.out.println(account.toString());
+//        }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         
     }
 }
